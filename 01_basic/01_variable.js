7e24ef309332730d74514e3acf06ce13e6d189a3
Bisho kumar <158822405+kumarBisho@users.noreply.github.com>
2025-08-16 04:47:58 +0000

    Datatype

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,12 @@
+const accountId = 12345
+let accountName = "Bisho kumar"
+var accountEmail = "bisho03@gmail.com"
+accountCity = "Patna"
+let accountState
+
+// print single variable
+console.log(accountId)
+// print multiple variable in a series 
+console.log(accountName, accountEmail, accountCity);
+// print multiple value in a table format
+console.table([accountId, accountName, accountEmail, accountCity, accountState])
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all js code as a newer version of js
+
+// alert(3 + 4) -> it can use only in browser in documented format. Here in node we can't use it.
+
+let name = "Bisho" // this is a string
+let age = 34  // this is a number. its range are 2 to the power 53
+let login = false // this is boolean type . true/false
+let job = null // it is a specific type of standalone value
+let state // this is undefined=> value not given(assigned
+
+// symbal => this is used to get unique structure 
+
+console.log(typeof name)
+console.log(typeof null) // null is a object type. null itself is not a type
+console.log(typeof undefined) // undefined itself is a type of datatype
