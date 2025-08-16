9016d0664655ccc16f42eac867e2dd7e96b58fa1
Bisho kumar <158822405+kumarBisho@users.noreply.github.com>
2025-08-16 12:46:55 +0000

    Date and Time explanation

---
+++ 01_basic/06_datetime.js
@@ -0,0 +1,34 @@
+
+// let myDate = new Date()
+
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toISOString())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleTimeString())
+// console.log(myDate.toJSON())
+
+// console.log(typeof myDate)
+
+// let createDate = new Date(2023, 4, 23)
+// let createDate = new Date(2023, 4, 23, 4,32)
+// let createDate = new Date("2023-01-4")
+// let createDate = new Date("05-07-2025")
+// console.log(createDate)
+// console.log(createDate.toLocaleString())
+
+// let myTime = Date.now()
+// console.log(myTime)
+// console.log(createDate.getTime())
+
+let newDate = new Date()
+
+// console.log(newDate)
+// console.log(newDate.getDate())
+// console.log(newDate.getDay())
+// console.log(newDate.getFullYear())
+
+newDate.toLocaleString('default',{
+    weekday: 'long',
+})
