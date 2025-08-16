7f641dfd8c1efeceb7bf5c8e0590384cc2d46dcf
Bisho kumar <158822405+kumarBisho@users.noreply.github.com>
2025-08-16 12:07:44 +0000

    Number and Math operation

---
+++ 01_basic/05_number.js
@@ -0,0 +1,32 @@
+// const score = 400
+// console.log(score)
+
+// const balance= new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+// const otherNumber = 134.5343
+// console.log(otherNumber.toPrecision(2))
+
+// const longNumber = 10000000
+// console.log(longNumber.toLocaleString('en-IN'))
+
+
+// ********************************  Maths  **********************************
+
+// console.log(Math)
+// console.log(Math.abs(-45))
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.3))
+// console.log(Math.floor(4.3))
+// console.log(Math.min(4,3,5,6,8))
+// console.log(Math.max(4,3,5,6,8))
+
+// console.log(Math.random())
+console.log(Math.floor((Math.random()*10)+1))
+
+// minVal = x1
+// maxVal = x2
+// formula : Math.floor((Math.random()*(maxVal-minVal+1)))+minVal
