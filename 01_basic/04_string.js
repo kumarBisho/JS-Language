ddb53f58e8697cf4bff7c434306f27bcdc7e3357
Bisho kumar <158822405+kumarBisho@users.noreply.github.com>
2025-08-16 11:35:04 +0000

    string operation

---
+++ 01_basic/04_string.js
@@ -0,0 +1,15 @@
+let name = "Bisho_kumar_yadav"
+
+let city = new String("Patna")
+
+// console.log(name)
+// console.log(city)
+
+// console.log(name.length)
+// console.log(name.toLowerCase())
+// console.log(name.toUpperCase())
+// console.log(name.replace("sh","k"))
+
+console.log(name.split('_'))
+console.log(name.slice(-6,-1))
+console.log(name.substring(-2,4))
